0f059c3bfb7b743cabd063817454528f62cbef23
Lucas Erlacher <l.erlacher@icloud.com>
2020-11-27 16:30:31 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now
+          Learn React from DEVELOPMENT
         </a>
       </header>
     </div>
