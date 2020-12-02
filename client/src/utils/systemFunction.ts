7fb6a50b697c34fac287481a9affcf6229bd90c6
tillschmidt <schmidt.till.mail@gmail.com>
2020-12-02 10:53:25 +0100

    start to merge login with firebase and redux

--- client/src/utils/systemFunction.ts
+++ client/src/utils/systemFunction.ts
@@ -1,12 +1,35 @@
 
 
+import fire from './fire';
 
-export const userLogin = () =>{  
-  
-  
+export const userLogin = (creds: any) => {
+    return (dispatch: any) => {
+        fire
+            .auth()
+            .signInWithEmailAndPassword(creds.email, creds.password)
+            .then(() => {
+                dispatch({type: "SIGN_IN"});
+            })
+            .catch(err => {
+                dispatch({ type : "SIGN_IN_ERR", err});
+            });
+    };
+};
+
+export const userLogOut = () => {
+    console.log("logout function called")
 }
-export const userLogOut = () =>{
-}
 
-
-export const userSignUp = () => {}
+export const userSignUp = (creds: any) => {
+    return (dispatch : any) => {
+        fire
+            .auth()
+            .createUserWithEmailAndPassword(creds.email, creds.password)
+            .then(() => {
+                dispatch({type: "SIGN_UP"});
+            })
+            .catch(err => {
+                dispatch({ type : "SIGN_UP_ERR", err});
+            });
+    };
+};
