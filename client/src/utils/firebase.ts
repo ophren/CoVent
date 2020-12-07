cb2301db1698f9380f5638c9eb0ff26d73b25d36
Benjamin <ophren_lied@live.fr>
2020-12-07 11:39:27 +0100

    hiding firebase config

--- client/src/utils/firebase.ts
+++ client/src/utils/firebase.ts
@@ -4,13 +4,7 @@ import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvb96JRq4x8H-sqCMXp3a-521kq9xh47w",
-    authDomain: "covent-2b7ac.firebaseapp.com",
-    databaseURL: "https://covent-2b7ac.firebaseio.com",
-    projectId: "covent-2b7ac",
-    storageBucket: "covent-2b7ac.appspot.com",
-    messagingSenderId: "16244326109",
-    appId: "1:16244326109:web:1fc139df113913abf9a534"
+    
   };
 
 
